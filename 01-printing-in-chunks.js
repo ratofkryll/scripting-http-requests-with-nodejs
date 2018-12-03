6b5189d31f9149ba3514585508d48001efa2ac54
Gioanna Rheumer <ratofkryll@gmail.com>
2018-12-03 20:05:25 +0000

    adds chunk concatenation

--- 01-printing-in-chunks.js
+++ 01-printing-in-chunks.js
@@ -13,7 +13,7 @@ function getAndPrintHTMLChunks () {
     response.setEncoding('utf8');
 
     response.on('data', function (data) {
-      console.log(data);
+      console.log(data + '\n');
     });
 
     response.on('end', function () {
